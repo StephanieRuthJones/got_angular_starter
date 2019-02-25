a30f17f6c93c725cc9777c98c6bde1140c2030ab
Nmuta Jones <nmuta.jones@Nmuta.local>
2019-02-25 12:44:43 -0700

    Initial lecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'westeros';
+  title:string = 'westeros';
+  season_8:boolean = false;
+
+  characters:string[] = ["Tyrian", "Deanerys", "The Hound", "Aryah Stark"];
+
+  counter:number = 0; 
+
+  bump(){
+     this.counter++;
+  }
+
+  reveal(){
+    this.season_8 = !this.season_8;
+  }
+
 }
